677a26e5bb957b119843648f764b0c163bd5486c
Sneha2k3 <125958986+Sneha2k3@users.noreply.github.com>
2025-07-30 16:52:21 +0545

    admin

--- backend/routes/adminRoute.js
+++ backend/routes/adminRoute.js
@@ -4,8 +4,8 @@ const Doctor = require("../models/doctorModel");
 const User = require("../models/userModel");
 const authMiddleware = require("../middlewares/authMiddleware");
 
-router.get("/get-all-doctors", authMiddleware, async(req, res) => {
-    try{
+router.get("/get-all-doctors", authMiddleware, async (req, res) => {
+    try {
         const doctors = await Doctor.find({});
         console.log(doctors);
         res.status(200).send({
@@ -13,7 +13,7 @@ router.get("/get-all-doctors", authMiddleware, async(req, res) => {
             success: true,
             data: doctors,
         });
-    }catch(error){
+    } catch (error) {
         console.log(error);
         res.status(500).send({
             message: "Error applying doctor account",
@@ -23,8 +23,8 @@ router.get("/get-all-doctors", authMiddleware, async(req, res) => {
     }
 });
 
-router.get("/get-all-users", authMiddleware, async(req, res) => {
-    try{
+router.get("/get-all-users", authMiddleware, async (req, res) => {
+    try {
         const users = await User.find({});
         console.log(users);
         res.status(200).send({
@@ -32,7 +32,7 @@ router.get("/get-all-users", authMiddleware, async(req, res) => {
             success: true,
             data: users,
         });
-    }catch(error){
+    } catch (error) {
         console.log(error);
         res.status(500).send({
             message: "Error applying doctor account",
@@ -42,41 +42,6 @@ router.get("/get-all-users", authMiddleware, async(req, res) => {
     }
 });
 
-router.post("/change-doctor-account-status", authMiddleware, async(req, res) => {
-    try{
-        const {doctorId, status} = req.body;
-        const doctor = await Doctor.findByIdAndUpdate(doctorId, {
-            status,
-        });
 
-        console.log(doctor);
-
-        const user = await User.findOne({_id: doctor.userId});
-
-        console.log(user);
-
-        const unseenNotifications = user.unseenNotifications;
-        unseenNotifications.push({
-            type: "new-doctor-request-changed",
-            message: `Your doctor account has been ${status}`,
-            onClickPath: "/notifications",
-        })
-        user.isDoctor = status === "approved" ? true : false;
-        await user.save();
-
-        res.status(200).send({
-            message: "Doctor status updated successfully",
-            success: true,
-            data: doctor,
-        });
-    }catch(error){
-        console.log(error);
-        res.status(500).send({
-            message: "Error applying doctor account",
-            success: false,
-            error,
-        });
-    }
-});
 
 module.exports = router;
